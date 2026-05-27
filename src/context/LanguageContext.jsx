5d7fab84b688629e16f7edddabc4b5a118edaaf9
Ansh-Badresiya <anshbadresiya284@gmail.com>
2026-05-28 00:38:16 +0530

    Change the defalut language from english to gujarati

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -4,7 +4,7 @@ import translations from '../translations';
 const LanguageContext = createContext();
 
 export function LanguageProvider({ children }) {
-  const [lang, setLang] = useState('en');
+  const [lang, setLang] = useState('gu');
   const t = translations[lang];
   const toggleLang = () => setLang(l => l === 'en' ? 'gu' : 'en');
   return (
